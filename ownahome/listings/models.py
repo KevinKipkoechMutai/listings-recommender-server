6468d80b5c41aa6343d7b876cfa0b167e279cbc1
kevinmutai <mutaikevinkip.05@gmail.com>
2024-11-25 21:14:54 +0300

    modify model to include image urls

--- ownahome/listings/models.py
+++ ownahome/listings/models.py
@@ -18,6 +18,7 @@ class Listing(models.Model):
     price_mortgage = models.DecimalField(max_digits=15, decimal_places=2)
     development_status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     description = models.TextField(blank=True, null=True)
+    image_url = models.URLField(max_length=500, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
