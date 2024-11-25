f27aaa2b18e5fba15fbd728b0fb78b4a0c963734
kevinmutai <mutaikevinkip.05@gmail.com>
2024-11-25 21:15:16 +0300

    configure endpoints

--- ownahome/listings/urls.py
+++ ownahome/listings/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
-from .views import list_listings, recommend_home
+from django.urls import path
+from .views import list_listings, recommend_listings, create_property, delete_listing
 
 urlpatterns = [
     path('listings/', list_listings, name='list_listings'),
-    path('recommend/', recommend_home, name='recommend_home'),
+    path('create-listing/', create_property, name='create_listing'),
+    path('recommend/', recommend_listings, name='recommend_listings'),
+    path('delete-listing/<int:listing_id>', delete_listing, name='delete_listings')
 ]
